ba1dd7f37f9969653bbcf06fd097deaab9ab533e
nzimba <nzimbabeni@yahoo.com>
2025-03-03 12:06:14 +0100

    Exercice 4 : Prenons l'application suivante qui ajoute des fonctionnalités supplémentaires à la dernière application décrite dans le dernier exercice. Quoi de neuf ? Comment pouvez-vous réduire la quantité de code dupliqué dans l'application en utilisant une expression réactive.

--- shiny/app.R
+++ shiny/app.R
@@ -14,8 +14,11 @@ ui <- fluidPage(
   # textOutput("gretting"),
   # tableOutput("mortgage")
   
-  sliderInput("x", label = "if x is", min =1, max = 50, value = 30), 
-  "then x time 5 is", textOutput("product")
+  sliderInput("x", label = "if x is", min =1, max = 50, value = 30),
+  sliderInput("y", label = "if x is", min = 1, max = 50, value = 5),
+  "then, (x * y) is ", textOutput("product"),
+  "and, (x * y) + 5 is ", textOutput("product_plus5"),
+  "and, (x * y) + 10 is", textOutput("product_plus10")
   )
 
 
@@ -23,7 +26,17 @@ ui <- fluidPage(
 server <- function(input,output,session){
   
   output$product <- renderText({
-    product <- input$x * 5
+    product <- input$x * input$y
+  })
+  
+  output$product_plus5 <- renderText({
+    product_plus5 <- (input$x * input$y)
+    product_plus5+5
+  })
+  
+  output$product_plus10 <- renderText({
+    product_plus10 <- (input$x * input$y)
+    product_plus10+10
   })
   
   # output$gretting <- renderText({
