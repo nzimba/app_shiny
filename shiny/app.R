762ccd2da23ea1f0e128aa86e77d2775c3b34fef
nzimba <nzimbabeni@yahoo.com>
2025-03-03 11:46:02 +0100

    Exercice 1 : Créez une application qui accueille l'utilisateur par son nom. Vous ne connaissez pas encore toutes les fonctions dont vous avez besoin pour faire cela, j'ai donc inclus quelques lignes de code ci-dessous. Réfléchissez aux lignes que vous utiliserez, puis copiez-les et collez-les au bon endroit dans une application Shiny.

--- shiny/app.R
+++ shiny/app.R
@@ -6,27 +6,39 @@ packageVersion("shiny")
 ##### User Interface
 
 ui <- fluidPage(
-  selectInput("dataset", label = "Dataset", choices= ls("package:datasets")),
-  verbatimTextOutput("summary"),
-  tableOutput("table")
-)
-
+  # selectInput("dataset", label = "Dataset", choices= ls("package:datasets")),
+  # verbatimTextOutput("summary"),
+  # tableOutput("table")
+  numericInput("age","How old are you?", value=NA),
+  textInput("name","What's your name?"),
+  textOutput("gretting"),
+  tableOutput("mortgage")
+  )
 
 
 ##### Server
 server <- function(input,output,session){
-  ## create a reactive expression
   
-    dataset <- reactive({
-      get(input$dataset, "package:datasets") 
-    })
-  output$summary <-renderPrint({
-    ### use a reactive expression by calling it like a function
-    summary(dataset())
+  output$gretting <- renderText({
+    paste0("Hello ", input$name)
   })
-  output$table <-renderTable({
-    dataset()
-  })
+  
+  # output$histogram <- renderPlot({
+  #   hist(rnorm(1000),res=96, input$age)
+  # })
+  
+  # ## create a reactive expression
+  # 
+  #   dataset <- reactive({
+  #     get(input$dataset, "package:datasets") 
+  #   })
+  # output$summary <-renderPrint({
+  #   ### use a reactive expression by calling it like a function
+  #   summary(dataset())
+  # })
+  # output$table <-renderTable({
+  #   dataset()
+  # })
 }
 
 #### run shiny application
