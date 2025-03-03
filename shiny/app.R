b0d30561a19935667bc60c165462bb5286e5e68f
nzimba <nzimbabeni@yahoo.com>
2025-03-03 12:24:25 +0100

    Exercice 5 : L'application suivante est très similaire à celle que vous avez vue plus tôt dans le chapitre : vous sélectionnez un ensemble de données à partir d'un package (cette fois, nous utilisons le package ggplot2 ) et l'application imprime un résumé et un tracé des données. Elle suit également les bonnes pratiques et utilise des expressions réactives pour éviter la redondance du code. Cependant, il y a trois bugs dans le code fourni ci-dessous. Pouvez-vous les trouver et les corriger ?

--- shiny/app.R
+++ shiny/app.R
@@ -1,7 +1,11 @@
 ### install packages
 
 library(shiny)
+library(ggplot2)
 packageVersion("shiny")
+
+datasets <- c("economics", "faithfuld", "seals")
+
 
 ##### User Interface
 
@@ -14,30 +18,44 @@ ui <- fluidPage(
   # textOutput("gretting"),
   # tableOutput("mortgage")
   
-  sliderInput("x", label = "if x is", min =1, max = 50, value = 30),
-  sliderInput("y", label = "if x is", min = 1, max = 50, value = 5),
-  "then, (x * y) is ", textOutput("product"),
-  "and, (x * y) + 5 is ", textOutput("product_plus5"),
-  "and, (x * y) + 10 is", textOutput("product_plus10")
+  # sliderInput("x", label = "if x is", min =1, max = 50, value = 30),
+  # sliderInput("y", label = "if x is", min = 1, max = 50, value = 5),
+  # "then, (x * y) is ", textOutput("product"),
+  # "and, (x * y) + 5 is ", textOutput("product_plus5"),
+  # "and, (x * y) + 10 is", textOutput("product_plus10")
+  
+  selectInput("dataset", "Dataset", choices = datasets),
+  verbatimTextOutput("summary"),
+  plotOutput("plot")
+  
   )
 
 
 ##### Server
 server <- function(input,output,session){
   
-  output$product <- renderText({
-    product <- input$x * input$y
+  dataset <- reactive({
+    get(input$dataset, "package:ggplot2")
   })
-  
-  output$product_plus5 <- renderText({
-    product_plus5 <- (input$x * input$y)
-    product_plus5+5
+  output$summary <- renderPrint({
+    summary(dataset())
   })
-  
-  output$product_plus10 <- renderText({
-    product_plus10 <- (input$x * input$y)
-    product_plus10+10
-  })
+  output$plot <- renderPlot({
+    plot(dataset())
+  }, res=96)
+  # output$product <- renderText({
+  #   product <- input$x * input$y
+  # })
+  # 
+  # output$product_plus5 <- renderText({
+  #   product_plus5 <- (input$x * input$y)
+  #   product_plus5+5
+  # })
+  # 
+  # output$product_plus10 <- renderText({
+  #   product_plus10 <- (input$x * input$y)
+  #   product_plus10+10
+  # })
   
   # output$gretting <- renderText({
   #   paste0("Hello ", input$name)
